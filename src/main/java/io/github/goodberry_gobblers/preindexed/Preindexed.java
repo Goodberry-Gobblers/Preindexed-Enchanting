472aef2cb63978dc37e892aebcd9965a02fbdedf
candyCalc <calansk0@gmail.com>
2025-11-18 06:15:26 -0800

    Add data driven max enchantment slots

--- src/main/java/io/github/goodberry_gobblers/preindexed/Preindexed.java
+++ src/main/java/io/github/goodberry_gobblers/preindexed/Preindexed.java
@@ -1,34 +1,45 @@
 package io.github.goodberry_gobblers.preindexed;
 
 import com.mojang.logging.LogUtils;
-import io.github.goodberry_gobblers.preindexed.config.CommonConfig;
+import com.mojang.serialization.Codec;
+import net.minecraft.core.Registry;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.data.DataProvider;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
+import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DataPackRegistryEvent;
 import org.slf4j.Logger;
+
+import java.util.Map;
+import java.util.Set;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Preindexed.MOD_ID)
-public class Preindexed
-{
+public class Preindexed {
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "preindexedenchanting";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
+    public static final ResourceKey<Registry<Map<String, Short>>> SLOTS_REGISTRY_KEY = ResourceKey.createRegistryKey(ResourceLocation.fromNamespaceAndPath(MOD_ID, "maxslots"));
+    public static final ResourceKey<Map<String, Short>> SLOTS_KEY = ResourceKey.create(SLOTS_REGISTRY_KEY, ResourceLocation.fromNamespaceAndPath(MOD_ID, "maxslots"));
+
+    public static final Codec<Map<String, Short>> SLOTS_MAP_CODEC = Codec.unboundedMap(Codec.STRING, Codec.SHORT);
+
     public Preindexed(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
-
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, CommonConfig.SPEC, "preindexed-enchanting.toml");
 
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
@@ -38,13 +49,121 @@ public class Preindexed
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
+
+        // gen data (for datapack specifically)
+        modEventBus.addListener(this::onGatherData);
+
+        //codec shit 4 datapack
+        modEventBus.addListener(this::registerDatapackRegistries);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
     }
 
-    // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+    }
+
+    public void registerDatapackRegistries(DataPackRegistryEvent.NewRegistry event) {
+        event.dataPackRegistry(
+                SLOTS_REGISTRY_KEY,
+                SLOTS_MAP_CODEC,
+                SLOTS_MAP_CODEC
+        );
+    }
+
+    //datagen shit :100:
+    public void onGatherData(GatherDataEvent event) {
+        event.getGenerator().addProvider(
+                event.includeServer(),
+                (DataProvider.Factory<DatapackBuiltinEntriesProvider>) output -> new DatapackBuiltinEntriesProvider(
+                        output,
+                        event.getLookupProvider(),
+                        new RegistrySetBuilder()
+                                .add(SLOTS_REGISTRY_KEY, bootstrap -> {
+                                    Map<String, Short> map = new java.util.HashMap<>(Map.of());
+                                    //  Defaults
+                                    //wood tier
+                                    map.put("minecraft:wooden_shovel", (short)5);
+                                    map.put("minecraft:wooden_pickaxe", (short)5);
+                                    map.put("minecraft:wooden_axe", (short)9);
+                                    map.put("minecraft:wooden_hoe", (short)4);
+                                    map.put("minecraft:wooden_sword", (short)8);
+                                    map.put("minecraft:leather_helmet", (short)5);
+                                    map.put("minecraft:leather_chestplate", (short)4);
+                                    map.put("minecraft:leather_leggings", (short)4);
+                                    map.put("minecraft:leather_boots", (short)5);
+                                    //stone tier
+                                    map.put("minecraft:stone_shovel", (short)0);
+                                    map.put("minecraft:stone_pickaxe", (short)0);
+                                    map.put("minecraft:stone_axe", (short)0);
+                                    map.put("minecraft:stone_hoe", (short)0);
+                                    map.put("minecraft:stone_sword", (short)0);
+                                    map.put("minecraft:chainmail_helmet", (short)0);
+                                    map.put("minecraft:chainmail_chestplate", (short)0);
+                                    map.put("minecraft:chainmail_leggings", (short)0);
+                                    map.put("minecraft:chainmail_boots", (short)0);
+                                    //iron tier
+                                    map.put("minecraft:iron_shovel", (short)-3);
+                                    map.put("minecraft:iron_pickaxe", (short)-3);
+                                    map.put("minecraft:iron_axe", (short)-3);
+                                    map.put("minecraft:iron_hoe", (short)-3);
+                                    map.put("minecraft:iron_sword", (short)-3);
+                                    map.put("minecraft:iron_helmet", (short)-3);
+                                    map.put("minecraft:iron_chestplate", (short)-3);
+                                    map.put("minecraft:iron_leggings", (short)-3);
+                                    map.put("minecraft:iron_boots", (short)-3);
+                                    //gold tier
+                                    map.put("minecraft:golden_shovel", Short.MAX_VALUE);
+                                    map.put("minecraft:golden_pickaxe", Short.MAX_VALUE);
+                                    map.put("minecraft:golden_axe", Short.MAX_VALUE);
+                                    map.put("minecraft:golden_hoe", Short.MAX_VALUE);
+                                    map.put("minecraft:golden_sword", Short.MAX_VALUE);
+                                    map.put("minecraft:golden_helmet", Short.MAX_VALUE);
+                                    map.put("minecraft:golden_chestplate", Short.MAX_VALUE);
+                                    map.put("minecraft:golden_leggings", Short.MAX_VALUE);
+                                    map.put("minecraft:golden_boots", Short.MAX_VALUE);
+                                    //diamond tier
+                                    map.put("minecraft:diamond_shovel", (short)9);
+                                    map.put("minecraft:diamond_pickaxe", (short)9);
+                                    map.put("minecraft:diamond_axe", (short)15);
+                                    map.put("minecraft:diamond_hoe", (short)7);
+                                    map.put("minecraft:diamond_sword", (short)14);
+                                    map.put("minecraft:diamond_helmet", (short)9);
+                                    map.put("minecraft:diamond_chestplate", (short)6);
+                                    map.put("minecraft:diamond_leggings", (short)8);
+                                    map.put("minecraft:diamond_boots", (short)9);
+                                    //netherite tier
+                                    map.put("minecraft:netherite_shovel", (short)10);
+                                    map.put("minecraft:netherite_pickaxe", (short)10);
+                                    map.put("minecraft:netherite_axe", (short)16);
+                                    map.put("minecraft:netherite_hoe", (short)8);
+                                    map.put("minecraft:netherite_sword", (short)15);
+                                    map.put("minecraft:netherite_helmet", (short)10);
+                                    map.put("minecraft:netherite_chestplate", (short)8);
+                                    map.put("minecraft:netherite_leggings", (short)10);
+                                    map.put("minecraft:netherite_boots", (short)11);
+                                    //misc
+                                    map.put("minecraft:fishing_rod", (short)8);
+                                    map.put("minecraft:flint_and_steel", (short)3);
+                                    map.put("minecraft:shears", (short)3);
+                                    map.put("minecraft:brush", (short)4);
+                                    map.put("minecraft:elytra", (short)4);
+                                    map.put("minecraft:carrot_on_a_stick", (short)3);
+                                    map.put("minecraft:warped_fungus_on_a_stick", (short)4);
+                                    map.put("minecraft:trident", (short)8);
+                                    map.put("minecraft:shield", (short)3);
+                                    map.put("minecraft:turtle_helmet", (short)9);
+                                    map.put("minecraft:bow", (short)6);
+                                    map.put("minecraft:crossbow", (short)8);
+
+                                    bootstrap.register(
+                                            SLOTS_KEY,
+                                            map
+                                    );
+                                }),
+                        Set.of(MOD_ID)
+                )
+        );
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
--- src/main/java/io/github/goodberry_gobblers/preindexed/mixins/ItemMixin.java
+++ src/main/java/io/github/goodberry_gobblers/preindexed/mixins/ItemMixin.java
@@ -1,16 +1,22 @@
 package io.github.goodberry_gobblers.preindexed.mixins;
 
 import io.github.goodberry_gobblers.preindexed.EnchantingSlots;
-import io.github.goodberry_gobblers.preindexed.config.CommonConfig;
+import io.github.goodberry_gobblers.preindexed.Preindexed;
 import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.Registry;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,6 +26,8 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 
 @Mixin(Item.class)
 public abstract class ItemMixin implements EnchantingSlots {
@@ -36,18 +44,38 @@ abstract
 class ItemStackMixin {
     @Shadow public abstract Item getItem();
 
+    @Shadow @Final private static Logger LOGGER;
+
+    @Shadow public abstract boolean isCorrectToolForDrops(BlockState pState);
+
     @Inject(method = "getTooltipLines", at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z", shift = At.Shift.AFTER, ordinal = 0), locals = LocalCapture.CAPTURE_FAILHARD)
-    public void getTooltipLines(Player pPlayer, TooltipFlag pIsAdvanced, CallbackInfoReturnable<List<Component>> cir, List list) {
-        if (this.getItem() != Items.ENCHANTED_BOOK) {
-            ChatFormatting style = ChatFormatting.BLUE;
+    public void getTooltipLines(Player pPlayer, TooltipFlag pIsAdvanced, CallbackInfoReturnable<List<Component>> cir, List<Component> list) {
+        Optional<Registry<Map<String, Short>>> slotsRegistry = Objects.requireNonNull(Minecraft.getInstance().getConnection()).registryAccess().registry(Preindexed.SLOTS_REGISTRY_KEY);
+        if (slotsRegistry.isPresent()) {
+            ResourceLocation location = ForgeRegistries.ITEMS.getKey(this.getItem());
+            if (location != null) {
+                Short maxSlots = Objects.requireNonNull(slotsRegistry.get().get(Preindexed.SLOTS_KEY)).get(location.toString());
+                if (maxSlots != null) {
+                    Integer usedSlots = EnchantingSlots.getUsedSlots((ItemStack) (Object) this);
 
-            Integer maxSlots = CommonConfig.DIAMON_SWORD_MAX_SLOTS.get();
-
-            list.add(Component.translatable(
-                    "item.preindexed.enchant_slots_tooltip",
-                    EnchantingSlots.getUsedSlots((ItemStack) (Object) this),
-                    maxSlots == -1 ? "\u221e": maxSlots
-            ).withStyle(style));
+                    if (maxSlots < 0) {
+                        list.add(Component.translatable(
+                                "item.preindexed.enchant_limit_tooltip",
+                                Math.abs(maxSlots)
+                        ).withStyle(ChatFormatting.DARK_PURPLE));
+                    } else if (maxSlots == 0) {
+                        list.add(Component.translatable(
+                                "item.preindexed.unenchantable_tooltip"
+                        ).withStyle(ChatFormatting.DARK_RED));
+                    } else {
+                        list.add(Component.translatable(
+                                "item.preindexed.enchant_slots_tooltip",
+                                usedSlots,
+                                maxSlots == Short.MAX_VALUE ? "\u221e" : maxSlots
+                        ).withStyle(ChatFormatting.BLUE));
+                    }
+                }
+            }
         }
     }
 }
