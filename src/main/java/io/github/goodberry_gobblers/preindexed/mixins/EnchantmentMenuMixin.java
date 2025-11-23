58f47ab28d9e96ea7e0bf436d2588e7d36169a89
candyCalc <calansk0@gmail.com>
2025-11-23 14:42:08 -0800

    fix unenchantable items in enchanting table

---
+++ src/main/java/io/github/goodberry_gobblers/preindexed/mixins/EnchantmentMenuMixin.java
@@ -0,0 +1,23 @@
+package io.github.goodberry_gobblers.preindexed.mixins;
+
+import io.github.goodberry_gobblers.preindexed.EnchantingSlotsHelper;
+import io.github.goodberry_gobblers.preindexed.Preindexed;
+import net.minecraft.world.inventory.EnchantmentMenu;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.Optional;
+
+@Mixin(EnchantmentMenu.class)
+public class EnchantmentMenuMixin {
+    @Redirect(method = "slotsChanged", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isEnchantable()Z"))
+    private boolean isEncahntable(ItemStack instance) {
+        Optional<Short> maxSlots = EnchantingSlotsHelper.getMaxSlots(instance, Preindexed.serverReference.overworld());
+        if (maxSlots.isPresent()) {
+            return instance.isEnchantable() && maxSlots.get() != Short.MIN_VALUE;
+        }
+        return instance.isEnchantable();
+    }
+}
